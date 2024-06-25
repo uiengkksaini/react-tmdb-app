ea06db85e323809e3c22e56c1eb3275d5f9fe539
Kishan Kumar Saini <kishan.saini@tavant.com>
2024-06-26 00:34:23 +0530

    base_url and api_key is secure now

--- src/tmdbAxios.ts
+++ src/tmdbAxios.ts
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const API_KEY = "7e6cb748e55608f80513396ce71a4752";
-const BASE_URL = "https://api.themoviedb.org/3";
-
 const tmdbAxios = axios.create({
-  baseURL: BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
   params: {
-    api_key: API_KEY,
+    api_key: process.env.REACT_APP_API_KEY,
   },
 });
 
